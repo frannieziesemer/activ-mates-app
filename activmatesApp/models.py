3183402970d1f8396e9931b97e4912abdbc1e5c2
frannie <frances.ziesemer@gmail.com>
2021-02-07 18:37:06 +0100

    add all forms and fix all forms functionality

--- activmatesApp/models.py
+++ activmatesApp/models.py
@@ -1,12 +1,5 @@
-from flask import Flask, render_template, url_for, flash, redirect
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, CreateProfileForm, LoginForm
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '36678eaa6a847166122011a8472d4f21'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+from activmatesApp import db
 
 
 class User(db.Model):
@@ -66,78 +59,3 @@ class ActivityType(db.Model):
 
     def __repr__(self):
         return f"Post('{self.number}', '{self.naem}')"
-
-
-# this would be called from the database
-activities = [
-    {
-        'author': 'Frannie Ziesemer',
-        'type': 'Running',
-        'description': '....',
-        'date_posted': 'February 1, 2021'
-    },
-    {
-        'author': 'Jonas Bischof',
-        'type': 'Tennis',
-        'description': '....',
-        'date_posted': 'February 1, 2021'
-    }
-]
-
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/landing-page')
-def landing_page():
-    return render_template('landing-page.html')
-
-
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Login')
-
-
-@app.route('/signup', methods=['GET', 'POST'])
-def sign_up():
-    registrationForm = RegistrationForm()
-    if registrationForm.validate_on_submit():
-        # alert message
-        flash(
-            f'Account created for {registrationForm.username.data}!', 'success')
-        # redirect on submit and validation # !!!!!here i want new profile section to show
-        # return redirect(url_for('home'))
-    createProfileForm = CreateProfileForm()
-    return render_template('sign-up.html',
-                           title='Sign Up',
-                           registrationForm=registrationForm,
-                           createProfileForm=createProfileForm
-                           )
-
-
-@app.route('/main-search')
-def main_search():
-    return render_template('main-search.html', title='Search', activities=activities)
-
-
-@app.route('/new-activity')
-def new_activity():
-    return render_template('new-activity.html', title='New Activity')
-
-
-@app.route('/profile')
-def profile():
-    return render_template('profile.html', title='Profile')
-
-
-@app.route('/view-activity')
-def view_activity():
-    return render_template('view-activity.html', title='View Activity')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ activmatesApp/routes.py
@@ -0,0 +1,92 @@
+from flask import render_template, url_for, flash, redirect
+from activmatesApp import app
+from activmatesApp.forms import RegistrationForm, CreateProfileForm, LoginForm
+from activmatesApp.models import User, Profile, Activity, ActivityType
+
+# this would be called from the database
+activities = [
+    {
+        'author': 'Frannie Ziesemer',
+        'type': 'Running',
+        'description': '....',
+        'date_posted': 'February 1, 2021'
+    },
+    {
+        'author': 'Jonas Bischof',
+        'type': 'Tennis',
+        'description': '....',
+        'date_posted': 'February 1, 2021'
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/landing-page')
+def landing_page():
+    return render_template('landing-page.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    loginForm = LoginForm()
+    error = None
+    if loginForm.validate_on_submit():
+        if loginForm.email.data == 'admin@blog.com' and loginForm.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('main_search'))
+        else: 
+            error = 'Login Unsuccessful. Please check username and password'
+    return render_template('login.html', 
+                            title='Login',
+                            loginForm=loginForm, 
+                            error=error
+                            )
+
+@app.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    registrationForm = RegistrationForm()
+    if registrationForm.validate_on_submit():
+        # alert message
+        flash(
+            f'Account created for {registrationForm.username.data}!', 'success')
+        return redirect(url_for('create_profile'))
+    return render_template('sign-up.html',
+                            title='Sign Up',
+                            registrationForm=registrationForm
+                           )
+    
+@app.route('/create_profile', methods=['GET', 'POST'])
+def create_profile():
+    """User sign up page"""
+    createProfileForm = CreateProfileForm()
+    #POST: sign user in 
+    if createProfileForm.validate_on_submit():
+        flash(
+            f'Profile created!', 'success')
+        return redirect(url_for('main_search'))
+    # GET: Serve Sign-up page
+    return render_template('create-profile.html', createProfileForm=createProfileForm)
+    
+@app.route('/main-search')
+def main_search():
+    return render_template('main-search.html', title='Search', activities=activities)
+
+
+@app.route('/new-activity')
+def new_activity():
+    return render_template('new-activity.html', title='New Activity')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', title='Profile')
+
+
+@app.route('/view-activity')
+def view_activity():
+    return render_template('view-activity.html', title='View Activity')
